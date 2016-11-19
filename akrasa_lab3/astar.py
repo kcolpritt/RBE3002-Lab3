15342ddd60496d36dd7f930cd5c51ffe2b6bcfb5
ajkrasa <ajkrasa@wpi.edu>
2016-11-19 15:27:22 -0500

    Fix Merge issues?
    Hopeing these changes will fix the merge issues I am having when tryig to merge my repo and the master one

--- akrasa_lab3/astar.py
+++ akrasa_lab3/astar.py
@@ -1,34 +1,72 @@
 #!/usr/bin/env python
-
 import rospy
-from astar import aStar
 from nav_msgs.msg import GridCells, Path
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Point, Pose, PoseStamped, PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry, OccupancyGrid
 from kobuki_msgs.msg import BumperEvent
+from tf.transformations import quaternion_from_euler
 import tf
 import numpy
-import math 
+import math
 import rospy, tf, numpy, math
 
 class Node:
-	def __init__(self,value,point):
+	def __init__(self,value, coor, start, goal, grid):
 		self.value = value
-		self.point = point
+		self.coor = coor
 		self.parent = None
-		self.H = 0
-		self.G = 0
-	def move_cost(self,other):
-		return 0 if self.value == '.' else 1
+		self.start = start
+		self.goal = goal
+		self.children = []
+		self.grid = grid
+		self.path = []
 		
-	def children(point,grid):
-		x,y = point.point
-		links = [grid[d[0]][d[1]] for d in [(x-1, y),(x,y - 1),(x,y + 1),(x+1,y)]]
-		return [link for link in links if link.value != '%']
+	def Children(self):
+		x = self.coor[0]
+		y = self.coor[1]
+		v = self.value
+		s = self.start
+		g = self.goal
+		m = self.grid
+		width = self.grid.info.width
+		height = self.grid.info.height
 
-	def manhattan(point,point2):
-		return abs(point.point[0] - point2.point[0]) + abs(point.point[1]-point2.point[0])
+		for i in range(0, height): #height should be set to hieght of grid
+			for j in range(0, width): #width should be set to width of grid
+				#print k # used for debugging
+				if (self.grid.data[i*width+j] == 100):
+					pass
+				elif(x+1 == i and y == j):
+					child1 = Node(v+1, (x+1, y), s, g, m)
+					self.children.append(child1)
+				elif(x == i and y+1 == j):
+					child2 = Node(v+1, (x, y+1), s, g, m)
+					self.children.append(child2)
+				elif(x-1 == i and y == j):
+					child3 = Node(v+1, (x-1, y), s, g, m)
+					self.children.append(child3)
+				elif(x == i and y-1 == j):
+					child4 = Node(v+1, (x, y-1), s, g, m)
+					self.children.append(child4)
+				else:
+					pass
+						
+
+	def Manhattan(self, point, point2):
+		return abs(point[0] - point2[0]) + abs(point[1]-point2[1])
+	
+	def Greedy(self):
+		x_side = abs(int(self.goal[0] - self.coor[0]))
+		y_side = abs(int(self.goal[1] - self.coor[1]))
+		hn = math.sqrt((x_side * x_side) + (y_side * y_side))
+		return hn
+
+	def CalF(self):
+		g = self.value
+		h = self.Greedy()
+		return g + h
+		
 
 def getLowestF(openset):
 	lowest = openSet[0]
@@ -37,95 +75,58 @@ def getLowestF(openset):
 			lowest = i
 	return lowest
 
-def aStar(start, goal, grid):
+
+def copyMap(data):
+	global mapGrid
+	global mapData
+	global width
+	global height
+	global mapgrid
 	global resolution
 	global offsetX
 	global offsetY
+	mapGrid = data
+	resolution = data.info.resolution
+	mapData = data.data
+	width = data.info.width
+	height = data.info.height
+	offsetX = data.info.origin.position.x
+	offsetY = data.info.origin.position.y
+	initGrid(mapGrid)
 
 
-	print "astar called"
-	#The open and closed sets
-	openset = set()
-	closedset = set()
-	#Current point is the starting point
+def Solve(start, goal, node):	
+	shortest = 0
+	current = node
 
-	j = int((current.x - offsetX - (.5 * resolution)) / resolution)
-	i = int((current.y - offsetY - (.5 * resolution)) / resolution)
+	while(current.coor != goal):
+		current.Children()
+		for i in current.children:
+			if(shortest == 0):
+				child = i
+				shortest = i.CalF()
+			elif(i.CalF() < shortest):
+				child = i
+				shortest = i.CalF()
+		child.parent.append(current)
+		child.path.append(current)
+		current = child
+	
+	current.path.append(current)
+	return current.path
+
+
+
+def aStar(start, goal, grid):
 	
 
-	print "start"
-	#Add the starting point to the open set
-	openset.add(current)
-	#While the open set is not empty
+	#initialX = start.pose.pose.position.x
+	#initialY = start.pose.pose.position.y
+	#initial = (initialX, initialY)
+	#endX = goal.pose.position.x
+	#endY = goal.pose.position.y
+	#end = (endX, endY)
 
-
-
-
-
-	print "In loop?"
-	while openset:
-		#Find the item in the open set with the lowest G + H score
-		current = getLowestF(openset)
-		#If it is the item we want, retrace the path and return it
-		if current == goal:
-			path = []
-			while current.parent:
-				path.append(current)
-				current = current.parent
-			path.append(current)
-			return path[::-1]
-		#Remove the item from the open set
-		openset.remove(current)
-		#Add it to the closed set
-		closedset.add(current)
-
-
-		print "here"
-
-		print children(current,grid)
-
-
-		#Loop through the node's children/siblings
-		for node in children(current,grid):
-			#If it is already in the closed set, skip it
-			if node in closedset:
-				continue
-			#Otherwise if it is already in the open set
-			if node in openset:
-				#Check if we beat the G score 
-				new_g = current.G + current.move_cost(node)
-				if node.G > new_g:
-					#If so, update the node to have a new parent
-					node.G = new_g
-					node.parent = current
-			else:
-				#If it isn't in the open set, calculate the G and H score for the node
-				node.G = current.G + current.move_cost(node)
-				node.H = manhattan(node, goal)
-				#Set the parent to our current item
-				node.parent = current
-				#Add it to the set
-				openset.add(node)
-	#Throw an exception if there is no path
-	raise ValueError('No Path Found')
-
-def next_move(pacman,food,grid):
-	#Convert all the points to instances of Node
-	for x in xrange(len(grid)):
-		for y in xrange(len(grid[x])):
-			grid[x][y] = Node(grid[x][y],(x,y))
-	#Get the path
-	path = aStar(grid[pacman[0]][pacman[1]],grid[food[0]][food[1]],grid)
-	#Output the path
-	print len(path) - 1
-	for node in path:
-		x, y = node.point
-		print x, y
-	pacman_x, pacman_y = [ int(i) for i in raw_input().strip().split() ]
-	food_x, food_y = [ int(i) for i in raw_input().strip().split() ]
-	x,y = [ int(i) for i in raw_input().strip().split() ] 
-	grid = []
-
-	for i in xrange(0, x):
-		grid.append(list(raw_input().strip()))
-		next_move((pacman_x, pacman_y),(food_x, food_y), grid)
+	origin = Node(0, start, start, goal, grid)
+	Solve(start, goal, origin)
+	
